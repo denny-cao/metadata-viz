8b0fc940eb65c625052fbf79797d6bcefe40ae25
Denny <dennycaopersonal@gmail.com>
2022-08-26 01:48:07 -0400

    change interpolation function and switch to exifread to read metadata

--- metadata.py
+++ metadata.py
@@ -1,49 +1,65 @@
 #!/usr/bin/python3
 
-from email.policy import default
-from typing import Tuple
+'''
+Extract metadata from raw files and visualize them to determine what lens to buy.
+'''
+
+'''
+TODO: 
+    - Add ability to save data to csv file
+    - Improve interpolation
+    - Add more metadata visualization
+    - Somehow factor in date of image... See what I favored in the past vs now
+    - Group similar focal legnths together to typical prime lens focal lengths
+'''
+
 import os 
 from numpy import linspace
-from scipy.interpolate import barycentric_interpolate
+from scipy.interpolate import interp1d
 import matplotlib.pyplot as plt
 from collections import defaultdict
-import exiftool
+import exifread
 
-def focal_length(img_path: str) -> float:
+def focal_length(img_path: str) -> int:
+    
     '''
     Get focal length of current image to add new key to focal length dictionary
     or increment value of present key 
     '''
-    with exiftool.ExifToolHelper() as ET:
-        metadata = ET.get_tag(filename=img_path, tag="FocalLength")
 
-    if metadata is None:
-        raise Exception("Make sure the file has metadata!")
-    else:
-        return metadata[10]
+    with open(img_path, 'rb') as raw:
+        tags = exifread.process_file(raw, stop_tag='FocalLength')
+        
+        return tags['EXIF FocalLength'].values[0]
 
 
-def interpolate(focal_len: list, focal_count: list, axis: linspace) -> list:
+def interpolate(focal_len: list, focal_count: list) -> list:
+   
     '''
     Interpolate discrete data points
     '''
 
-    return barycentric_interpolate(focal_len, focal_count, axis)
+    return interp1d(focal_len, focal_count, kind='quadratic')
     
 
 def plot(focal_dict: defaultdict) -> None:
+    
     '''
     Parse dictionary and split key (focal length) and value (count)
     and plot key as x and value as y
     '''
-    x_discrete = focal_dict.keys
-    y_discrete = focal_dict.values
 
-    x = linspace(min(x_discrete), max(x_discrete), num=100)
-    interpolated_y = interpolate(x_discrete, y_discrete, x)
+    x_discrete = list(int(x) for x in focal_dict.keys())
+    y_discrete = list(focal_dict.values())
+    
+    x_new = linspace(min(x_discrete), max(x_discrete), num=100)
+    
+    interpolated_y = interp1d(x_discrete, y_discrete, kind='quadratic')
+    y_smooth = interpolated_y(x_new)
+
 
     plt.plot(x_discrete, y_discrete, "o", label="discrete")
-    plt.plot(x, interpolated_y, label="barycentric interpolation")
+    plt.plot(x_new, y_smooth, label="quadratic interpolation")
 
     plt.legend()
     plt.show()
@@ -52,15 +68,16 @@ def plot(focal_dict: defaultdict) -> None:
 if __name__ == "__main__":
     path = "/mnt/f/My Drive/Photography/2022"
 
-    focal_lengths = defaultdict(float)
+    focal_lengths = defaultdict(int)
 
     for root, dirs, files in os.walk(path, topdown=False):
         for name in files:
             if name.endswith('.NEF'):
                 foc_len = focal_length(os.path.join(root, name))
                 focal_lengths[foc_len] += 1
-    
+
     plot(focal_lengths)
     
+
 
 
