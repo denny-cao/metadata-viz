99d42843a258d95faf7c5563ebc7bc34ba847187
Denny <dennycaopersonal@gmail.com>
2022-09-04 16:35:07 -0400

    create metadata class

--- metadata.py
+++ metadata.py
@@ -11,6 +11,7 @@ TODO:
     - Add more metadata visualization
     - Somehow factor in date of image... See what I favored in the past vs now
     - Group similar focal legnths together to typical prime lens focal lengths
+    - Dictionary of dictionaries to store metadata
 '''
 
 import os 
@@ -21,67 +22,93 @@ from collections import defaultdict
 import exifread
 
 
-def focal_length(img_path: str) -> int:
-    
-    '''
-    Get focal length of current image to add new key to focal length dictionary
-    or increment value of present key 
-    '''
+class Metadata:
+    def __init__(self):
+        self.img_path = None
+        self.x, self.y = [], []
+        self.meta_dict = defaultdict(lambda: defaultdict(int))
 
-    with open(img_path, 'rb') as raw:
-        tags = exifread.process_file(raw, stop_tag='FocalLength')
+
+    def update_img_path(self, img_path):
+        self.img_path = img_path
+
+
+    def interpolate(self) -> interp1d:
+    
+        '''
+        Interpolate discrete data points
+        '''
         
-        return tags['EXIF FocalLength'].values[0]
+        interpolated_y = interp1d(self.x, self.y, kind='quadratic')
+
+        x_new = linspace(min(self.x), max(self.x), num=100)
+        
+        return x_new, interpolated_y(x_new)
 
 
-def interpolate(focal_len: list, focal_count: list) -> interp1d:
-   
-    '''
-    Interpolate discrete data points
-    '''
+    def focal_length(self) -> int:
+        
+        '''
+        Get focal length of current image to add new key to metadata dictionary
+        or increment value of present key 
+        '''
+
+        with open(self.img_path, 'rb') as raw:
+            tags = exifread.process_file(raw, stop_tag='FocalLength')
+            
+            return tags['EXIF FocalLength'].values[0]
+
     
-    interpolated_y = interp1d(focal_len, focal_count, kind='quadratic')
+    def apertures(self) -> float:
+        
+        '''
+        Get aperature of current image to add new key to metadata dictionary
+        or increment value of present key
+        '''
 
-    x_new = linspace(min(focal_len), max(focal_len), num=100)
-    return x_new, interpolated_y(x_new)
+        with open(self.img_path, 'rb') as raw:
+            tags = exifread.process_file(raw, stop_tag='ApertureValue')
+            
+            return tags['EXIF ApertureValue'].values[0]
 
-def plot(focal_dict: defaultdict) -> None:
     
-    '''
-    Parse dictionary and split key (focal length) and value (count)
-    and plot key as x and value as y
-    '''
+    def plot(self) -> None:
+        
+        '''
+        Parse dictionary and split key (metadata) and value (count)
+        and plot key as x and value as y
+        '''
 
-    x_discrete, y_discrete = zip(*focal_dict.items())
-    x_discrete = list(int(x) for x in x_discrete)
-    y_discrete = list(y_discrete)
+        x_discrete, y_discrete = zip(*self.meta_dict['Focal Lengths'].items())
+        x_discrete = list(int(x) for x in x_discrete)
+        y_discrete = list(y_discrete)
 
-    x_new, y_smooth = interpolate(x_discrete, y_discrete)
+        self.x, self.y = x_discrete, y_discrete
+        x_new, y_smooth = self.interpolate()
 
-    plt.plot(x_discrete, y_discrete, "o", label="discrete")
-    plt.plot(x_new, y_smooth, label="quadratic interpolation")
+        plt.plot(x_discrete, y_discrete, 'o', label='discrete')
+        plt.plot(x_new, y_smooth, label='quadratic interpolation')
 
-    plt.legend()
-    plt.show()
+        plt.legend()
+        plt.show()
 
 
 def main() -> None:
-    path = "/mnt/f/My Drive/Photography/2022"
-
-    focal_lengths = defaultdict(int)
+    metadata = Metadata()
+    path = '/mnt/f/My Drive/Photography/2022'
 
     for root, dirs, files in os.walk(path, topdown=False):
         for name in files:
             if name.endswith('.NEF'):
-                foc_len = focal_length(os.path.join(root, name))
-                focal_lengths[foc_len] += 1
+                metadata.update_img_path(os.path.join(root, name))
+                foc_len = metadata.focal_length()
+                metadata.meta_dict['Focal Lengths'][foc_len] += 1
 
-    plot(focal_lengths)
+    metadata.plot()
 
 
-if __name__ == "__main__":
-    main()
-    
+if __name__ == '__main__':
+    main()   
 
 
 
